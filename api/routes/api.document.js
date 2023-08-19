8fc21945fb66e89cb6130c5e34575a920df1e1a4
Chirag-creator17 <chiraggoel172002@gmail.com>
2023-08-19 19:41:45 +0530

    fetch documents

--- api/routes/api.document.js
+++ api/routes/api.document.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require('../controller/program.js')
 
 router.post('/create', controller.createDocument)
-router.get('/fetch', controller.fetchDocument)
+router.post('/fetch', controller.fetchDocument)
 
 module.exports = router
--- client/src/components/dashboard/UserDashComponent.jsx
+++ client/src/components/dashboard/UserDashComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -6,15 +6,44 @@ import Col from "react-bootstrap/Col";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import "./styles.css";
+
 export const UserDashComponent = () => {
   const navigate = useNavigate();
   const name = localStorage.getItem("name");
   const hid = localStorage.getItem("health_id");
+  const role = localStorage.getItem("role");
+  const [documents, setDocuments] = useState([]);
   useEffect(() => {
     if (localStorage.getItem("auth_token") === null) {
       navigate("/");
     }
   }, []);
+  async function fetchData() {
+    try {
+      const data = {
+        id: hid,
+        profileType: role,
+      };
+      await fetch("http://localhost:6969/api/document/fetch", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data.documents);
+          setDocuments(data.documents);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Container>
       <Row>
@@ -35,22 +64,23 @@ export const UserDashComponent = () => {
         <Table>
           <thead>
             <tr>
+              <th>Document id</th>
               <th>Name</th>
               <th>Date</th>
-              <th>Record</th>
+              <th>Description</th>
+              <th>Document</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Chirag</td>
-              <td>12/12/12</td>
-              <td>Record</td>
-            </tr>
-            <tr>
-              <td>Chirag</td>
-              <td>12/12/12</td>
-              <td>Record</td>
-            </tr>
+            {documents.map((document) => (
+              <tr key={document.publicKey}>
+                <td>{document.publicKey}</td>
+                <td>{document.account.data}</td>
+                <td>{document.account.timestamp}</td>
+                <td>{document.account.description}</td>
+                <td>{<document className="account uri"></document> }</td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </Row>
